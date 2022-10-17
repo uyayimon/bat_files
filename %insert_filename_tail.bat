f548816b5857204d0a1fde0f7243eafb891108c8
uyayimon <mmjhmk@gmail.com>
2022-10-18 01:36:31 +0900

    [add] 2 insert-to-filename bat

---
+++ %insert_filename_tail.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayexpansion
+
+if "%~1"=="" (
+  exit /b
+)
+
+set x=%*
+
+
+echo "拡張子の前に文字列が挿入されます"
+echo "ファイル名・パスにスペースを含んでいるとエラーになる（解決法探します）"
+
+set /p insert=insert string:
+
+for %%I in (!x!) do call :sub "%%I"
+
+:sub
+echo %~1
+set filename_bef=%~n1
+echo %filename_bef%
+set extension=%~x1
+echo %extension%
+
+set filename_aft=%filename_bef%%insert%%extension%
+echo %filename_aft%
+
+rename %1 %filename_aft%
+
+
+endlocal
---
+++ %insert_now_date_tail.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayexpansion
+
+if "%~1"=="" (
+  exit /b
+)
+
+set x=%*
+
+set today=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%
+
+set time2=%TIME: =0%
+set now=%time2:~0,2%%time2:~3,2%%time2:~6,2%
+
+for %%I in (!x!) do call :sub "%%I"
+
+
+:sub
+echo %~1
+set filename_bef=%~n1
+set extension=%~x1
+
+rem _YYYYMMDD
+set filename_aft=%filename_bef%_%today%%extension%
+rem _YYYYMMDD_hhmmss
+@REM set filename_aft=%filename_bef%_%today%_%now%%extension%
+
+echo %filename_aft%
+pause
+
+rename %~1 %filename_aft%
+
+endlocal
